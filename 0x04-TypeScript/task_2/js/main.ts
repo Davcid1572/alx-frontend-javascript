a5e0550cdebf5f0ae795d5cc290056c88fdb6143
Davcid1572 <ifejiofordavid1572@gmail.com>
2025-11-09 14:18:14 +0100

    .

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -42,18 +42,6 @@ function createEmployee(salary: number | string): Director | Teacher {
   }
 }
 
-function isDirector(employee: Director | Teacher): employee is Director {
-  return (employee as Director).workDirectorTasks !== undefined;
-}
-
-function executeWork(employee: Director | Teacher): string {
-  if (isDirector(employee)) {
-    return employee.workDirectorTasks();
-  } else {
-    return employee.workTeacherTasks();
-  }
-}
-
 // Example usage
 const emp1 = createEmployee(200); // Teacher
 const emp2 = createEmployee(1000); // Director
